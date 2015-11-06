eb522b51130515fb593ab94f61250c2acde0da95
EG58 <EG58@users.noreply.github.com>
2015-11-05 18:36:32 -0800

    Create b.h

---
+++ src/b.h
@@ -0,0 +1,22 @@
+#include <string.h>
+#include <string>
+
+using namespace std;
+
+class hash_time 
+{
+	public:
+		string _hash_tag ; 
+		string _time ;
+
+};
+
+class edge_time 
+{
+	public:
+		string _tag1 ;
+
+		string _tag2 ; 
+
+		string _time ; 
+};
