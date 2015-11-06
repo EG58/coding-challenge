88c3b81c548e03656c151fac2f0f373bfdb9bf52
EG58 <EG58@users.noreply.github.com>
2015-11-05 18:38:44 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
-#!/usr/bin/env bash
 
-# example of the run script for running the word count
 
-# I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
 g++ tweets_cleaned.cpp
+
+
 g++ average_degree.cpp
 
 
